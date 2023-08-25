131d216a4b83b227a0d3fbd63256a5f75c7d163e
lettyfranca <leeffrance@gmail.com>
2023-08-25 09:10:52 -0300

    alterações da classe conta e main

--- Projeto/Main.py
+++ Projeto/Main.py
@@ -5,9 +5,14 @@ class Main:
 #importação da classe Cliente
 from Cliente import Cliente
 
+#importação da classe Conta
+from Conta import Conta
+
 #instanciação de um novo objeto
 c1 = Cliente("Letícia", "1199999-9999")
+conta = Conta(c1.nome, 3101, 0)
 
 #impressão do resultado
 print(c1)
 print(c1.nome, "e", c1.telefone)
+print(conta.titular, "Número:", conta.numero, "Saldo =", conta.saldo)
