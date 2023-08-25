6978049cd9b51d29228e176029f3c49cdc73a9c7
lettyfranca <leeffrance@gmail.com>
2023-08-25 09:05:36 -0300

    Classe cliente

--- Projeto/Main.py
+++ Projeto/Main.py
@@ -1,1 +1,13 @@
-print("Testando o Projeto")
+#A classe Main não possui atributos. Utilizamos apenas a palavra reservada pass.
+class Main:
+    pass
+
+#importação da classe Cliente
+from Cliente import Cliente
+
+#instanciação de um novo objeto
+c1 = Cliente("Letícia", "1199999-9999")
+
+#impressão do resultado
+print(c1)
+print(c1.nome, "e", c1.telefone)
